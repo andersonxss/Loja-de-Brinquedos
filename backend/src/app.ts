95780777442573f74fd31b9c8caea83a0c8e7165
andersonxss@hotmail.com <andersonxss@hotmail.com>
2025-08-05 21:41:18 -0300

    Documentando o projeto com swagger.json

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -2,6 +2,8 @@ import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
 import routes from "./routes";
+import swaggerUi from "swagger-ui-express";
+import swaggerDocument from "./swagger.json";
 import { errorHandler } from "./middlewares/errorHandler";
 
 dotenv.config();
@@ -10,9 +12,12 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
+
+// Configurar Swagger primeiro (antes das rotas da API)
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+
+// Rotas da API
 app.use("/api", routes);
 app.use(errorHandler);
-
-// Rotas serão adicionadas aqui
 
 export default app;
